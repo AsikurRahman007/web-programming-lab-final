c2497c3d0c5716dfcdf3d0e12e51acf1bfd10acb
Asikur Rahman Anik/Smuct <asikurrahman023@gmail.com>
2026-01-14 14:20:17 +0600

    Add Privateroutes

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import { useContext } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
+import Loading from "../components/Loading/Loading";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+
+  if (loading) {
+    return <Loading></Loading>;
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to={"/auth/login"}></Navigate>;
+};
+
+PrivateRoute.propTypes = {
+  children: PropTypes.object,
+};
+
+export default PrivateRoute;
