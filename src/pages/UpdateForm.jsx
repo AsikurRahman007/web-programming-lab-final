effb86901eecceaaccd916c9806076718f80e152
Asikur Rahman Anik/Smuct <asikurrahman023@gmail.com>
2026-01-23 10:21:20 +0000

    added Updatefrom jsx

---
+++ src/pages/UpdateForm.jsx
@@ -0,0 +1,25 @@
+import { useLoaderData } from "react-router-dom";
+import InputForm from "../components/InputForm/InputForm";
+
+const UpdateForm = () => {
+  const productData = useLoaderData();
+
+  return (
+    <div className="border-4 border-black pt-10 lg:pt-20 w-5/6 mx-auto">
+      <div>
+        <h1 className="text-3xl md:text-5xl lg:text-6xl text-black font-bold text-center">
+          Update Item
+        </h1>
+        <p className="text-base md:text-lg lg:text-xl  text-center mt-6 w-5/6 lg:w-3/4 mx-auto">
+          It is a long established fact that a reader will be distraceted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using Content here.
+        </p>
+      </div>
+      <InputForm productData={productData}></InputForm>
+    </div>
+  );
+};
+
+export default UpdateForm;
