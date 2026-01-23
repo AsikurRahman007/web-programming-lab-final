f9be3243d1d259da7bc9ce9c6e440af44bef172a
Asikur Rahman Anik/Smuct <asikurrahman023@gmail.com>
2026-01-23 15:47:41 +0000

    Added Boots

---
+++ src/components/FootballBoots/FootballBoots.jsx
@@ -0,0 +1,16 @@
+import { useLoaderData } from "react-router-dom";
+import ProductCard from "../ProductCard/ProductCard";
+
+const FootballBoots = () => {
+  const footballBoots = useLoaderData();
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
+      {footballBoots.map((product) => (
+        <ProductCard key={product._id} product={product}></ProductCard>
+      ))}
+    </div>
+  );
+};
+
+export default FootballBoots;
