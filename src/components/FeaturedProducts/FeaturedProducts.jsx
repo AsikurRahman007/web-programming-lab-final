e73e6026463d4bd4bdbee6c57afb35f63459b8c2
Asikur Rahman Anik/Smuct <asikurrahman023@gmail.com>
2026-01-23 15:42:59 +0000

    Added FeaturedProdect

---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,18 @@
+const FeaturedProducts = () => {
+  return (
+    <div>
+      <div className="relative py-4">
+        <div className="absolute inset-0 flex items-center">
+          <div className="w-full border-b-4 border-black"></div>
+        </div>
+        <div className="relative flex justify-center">
+          <span className="bg-white px-4 text-black text-2xl md:text-3xl lg:text-5xl font-bold">
+            FEATURED PRODUCTS
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
